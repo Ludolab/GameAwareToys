0ad720d6578a96d530c86abae6028a215d465984
Erik Harpstead <eharpste@cs.cmu.edu>
2022-07-12 15:51:26 -0400

    Updated TrackableWalker
    Renamed the file from TackableWalker to TrackableWalker. Also added a number of new features:
    color, and state are added to the keyFrame data.
    next waypoint destination added to keyframedata.
    updated the WalkCycle code to be smoother
    added a few more names to the secret name list.

--- Game Aware Toys/Assets/Scripts/TrackableWalker.cs
+++ Game Aware Toys/Assets/Scripts/TrackableWalker.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 using GameAware;
 using Newtonsoft.Json.Linq;
 
-public class TackableWalker : MetaDataTrackable {
+public class TrackableWalker : MetaDataTrackable {
     //Tryin to evoke a similar energy to the Pac-Man Ghost names
     public static string[] Walker_Names = {
-        "Walky", "Blocky", "Chalky", "Pocky", "Talky", "Mocky", "Gawky", "Flocky", "Rocky", "Stocky", "Clyde"
+        "Walky", "Blocky", "Chalky", "Docky", "Flocky", "Gawky", "Hockey",
+        "Jocky", "Knocky", "Locky", "Mocky", "Pocky",  "Rocky", "Stocky", "Talky", "Clyde"
     };
 
+    private int currentWaypointDex;
+
     public string secretName = string.Empty;
+    public string color = string.Empty;
     public Vector2[] waypoints;
-    public int currentWaypointDex;
-    public float speed;
-    public float waitTimeAtPoints;
+    public float moveSpeed;
+    public float timeAtPoints;
 
     public enum WalkingState {
         Idle,
@@ -36,13 +39,16 @@ public class TackableWalker : MetaDataTrackable {
 
     IEnumerator WalkCycle() {
         while (walkingState != WalkingState.Idle) {
-            while ((waypoints[currentWaypointDex] - (Vector2)transform.position).magnitude > .0001) {
-                Vector2 direction = waypoints[currentWaypointDex] - (Vector2)transform.position;
-                transform.position += (Vector3)(direction.normalized * speed * Time.deltaTime);
+            float startMoveTime = Time.time;
+            Vector3 originalPosition = transform.position;
+            float moveTime = Vector2.Distance(originalPosition, waypoints[currentWaypointDex]) / moveSpeed;
+            while (Time.time - startMoveTime < moveTime) {
+                transform.position = Vector3.Lerp(originalPosition, waypoints[currentWaypointDex],
+                    (Time.time - startMoveTime) / moveTime);
                 yield return new WaitForEndOfFrame();
             }
             walkingState = WalkingState.Waiting;
-            yield return new WaitForSeconds(waitTimeAtPoints);
+            yield return new WaitForSeconds(timeAtPoints);
             currentWaypointDex += 1;
             currentWaypointDex %= waypoints.Length;
             walkingState = WalkingState.Walking;
@@ -53,6 +59,18 @@ public class TackableWalker : MetaDataTrackable {
     public override JObject KeyFrameData() {
         JObject job =  base.KeyFrameData();
         job["secret_name"] = secretName;
+        job["color"] = color;
+        job["state"] = walkingState.ToString();
+        job["next_waypoint"] = ScreenSpaceHelper.ScreenPosition(waypoints[currentWaypointDex]).toJObject();
         return job;
+    }
+
+    public override JObject InbetweenData() {
+        if (walkingState == WalkingState.Walking) {
+            return base.InbetweenData();
+        }
+        else {
+            return new JObject();
+        }
     }
 }
