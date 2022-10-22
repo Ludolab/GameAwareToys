a87563579037b6bcf8133cb5de157dd1ccbcff3c
Erik Harpstead <eharpste@cs.cmu.edu>
2022-10-22 11:39:00 -0400

    Expanded moving camera toy
    Added a few more moving camera modes that will be better test cases for the system. The rotate mode cycles through rotating each of the individual axes, and the zoom mode zooms the camera in and out. Unlike panning these also have implications on the visual bounds of objects.

--- Game Aware Toys/Assets/Scripts/Walkers/WalkingCamera.cs
+++ Game Aware Toys/Assets/Scripts/Walkers/WalkingCamera.cs
@@ -1,12 +1,37 @@
+using System.ComponentModel.Composition;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WalkingCamera : MonoBehaviour {
 
+    public enum CameraMovement {
+        Pan,
+        Rotate,
+        Zoom
+
+    }
+
+    [Header("Cycle Settings")]
+    public float cylceTime;
+    public CameraMovement movement;
+
+
+    [Header("Pan Settings")]
     public float maxDistance;
     public float numRotations;
-    public float cylceTime;
+
+    [Header("Rotate Settings")]
+    public float maxAngle;
+
+    [Header("Rotate Settings")]
+    public float minZoom;
+    public float maxZoom;
+
+    private int rotationAxis = 0;
     private float lastSwitch;
     private bool goingOut = true;
+
+
 
 
     // Start is called before the first frame update
@@ -16,6 +41,26 @@ public class WalkingCamera : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        switch (movement) {
+            case CameraMovement.Pan:
+                transform.rotation = Quaternion.identity;
+                Camera.main.orthographicSize = 5;
+                PanUpdate();
+                break;
+            case CameraMovement.Rotate:
+                transform.position = new Vector3(0, 0, -10);
+                Camera.main.orthographicSize = 5;
+                RotateUpdate();
+                break;
+            case CameraMovement.Zoom:
+                transform.position = new Vector3(0, 0, -10);
+                transform.rotation = Quaternion.identity;
+                ZoomUpdate();
+                break;
+        }
+    }
+
+    void PanUpdate() {
         float t = (Time.time - lastSwitch) / cylceTime;
         float angle;
         float distance;
@@ -33,6 +78,48 @@ public class WalkingCamera : MonoBehaviour {
         pos.z = transform.position.z;
         transform.position = pos;
 
+        if (Time.time - lastSwitch > cylceTime) {
+            goingOut = !goingOut;
+            lastSwitch = Time.time;
+        }
+    }
+
+    void RotateUpdate() {
+
+        float t = (Time.time - lastSwitch) / cylceTime;
+        float angle = maxAngle * Mathf.Sin(t * 2 * Mathf.PI);
+
+        Quaternion rot = rotationAxis switch {
+            0 => Quaternion.Euler(angle, 0, 0),
+            1 => Quaternion.Euler(0, angle, 0),
+            2 => Quaternion.Euler(0, 0, angle),
+            _ => Quaternion.Euler(angle, 0, 0)
+        };
+
+        transform.position = rot * Vector3.back * 10;
+        transform.LookAt(Vector3.zero);
+        if (rotationAxis == 2) {
+            transform.rotation = rot;
+        }
+
+        if (Time.time - lastSwitch > cylceTime) {
+            transform.rotation = Quaternion.identity;
+            rotationAxis++;
+            rotationAxis %= 3;
+            lastSwitch = Time.time;
+        }
+    }
+
+    void ZoomUpdate() {
+        float t = (Time.time - lastSwitch) / cylceTime;
+        t = (Mathf.Sin(t * 2 * Mathf.PI) + 1) / 2;
+        if (goingOut) {
+            Camera.main.orthographicSize = Mathf.Lerp(minZoom, maxZoom, t);
+        }
+        else {
+            Camera.main.orthographicSize = Mathf.Lerp(maxZoom, minZoom, t);
+        }
+        
         if (Time.time - lastSwitch > cylceTime) {
             goingOut = !goingOut;
             lastSwitch = Time.time;
