f3c4d10899f16d4133f7297bc90d535dfe5f88d3
Erik Harpstead <eharpste@cs.cmu.edu>
2022-10-29 15:41:03 -0400

    Update MetaDataTracker.cs
    Re-organized and streamlined the class structure. Some key changes:
    
    - There is now a function that can be called to start the system without having to activate it on start
    - metadata times are now sent as an integer of milliseconds to align with how browsers encode time.
    - distinctions between being Connected and Recording are more clearly defined

--- Game Aware Toys/Assets/Scripts/GameAwareCore/MetaDataTracker.cs
+++ Game Aware Toys/Assets/Scripts/GameAwareCore/MetaDataTracker.cs
@@ -13,6 +13,17 @@ namespace GameAware {
     public class MetaDataTracker : MonoBehaviour {
         public static MetaDataTracker Instance { get; private set; } = null;
 
+        private const string LATEST_FRAME = "latest";
+        private const string START_FRAME = "start_frame";
+        private const string END_FRAME = "end_frame";
+        private const string PUB_SUB_CHANNEL = "server_control";
+
+        public enum RecordingUpdate {
+            Update,
+            LateUpdate,
+            FixedUpdate
+        }
+
         private List<IMetaDataTrackable> keyItems = new List<IMetaDataTrackable>();
         private List<IMetaDataTrackable> tweenItems = new List<IMetaDataTrackable>();
         private List<IMetaDataTrackable> newItems = new List<IMetaDataTrackable>();
@@ -24,50 +35,32 @@ namespace GameAware {
         private JArray tweens = new JArray();
         private JObject currentFrameData = new JObject();
 
+        private ConnectionMultiplexer redisConn;
+        private IDatabase redDb = null;
+
         [Tooltip("The URL of the Middleware Redis service")]
         public string middleWareURI = "localhost";
+        
         [Tooltip("The port used by the Middleware Redis service. The conventional Redis port is 6379.")]
         public int middleWarePort = 6379;
+        
         [Tooltip("The number of seconds used to keep the connection to the Middleware service alive")]
         public int keepAliveTime = 180;
+        
         [Tooltip("Password for the Middleware Redis service")]
         public string middleWareRedisPassword = "changeme";
+        
         [Tooltip("True = a connection to the MiddleWare service will be made on Start. False = a connection to the Middleware service must be triggered at some point later.")]
-        public bool connectOnStart = false;
-
-        public bool recording = false;
-
+        public bool BeginMetaDataOnStart = false;
+        
         [Tooltip("Name of your game, hardcoded for the start message")]
         public string gameName = "";
+
         [Tooltip("Name of the streamer, ideally set before connection")]
         public string streamerName = "";
 
-        public JsonSerializerSettings serializerSettings;
-
-        private ConnectionMultiplexer redisConn;
-        private IDatabase redDb = null;
-        private const string LATEST_FRAME = "latest";
-        private const string START_FRAME = "start_frame";
-        private const string END_FRAME = "end_frame";
-        private const string PUB_SUB_CHANNEL = "server_control";
-        
-
-        public enum RecordingUpdate {
-            Update,
-            LateUpdate,
-            FixedUpdate
-        }
-
         [Tooltip("Which of Unity's update loops should be used to record data. This is more for making it easy to do testing between the options for now. I don't think this setting will make it into the final version.")]
         public RecordingUpdate updateMode = RecordingUpdate.Update;
-
-        
-
-        private float CurrentTime {
-            get {
-                return updateMode == RecordingUpdate.FixedUpdate ? Time.fixedTime : Time.time;
-            }
-        }
 
         [Tooltip("The number of key frames per second")]
         public float keyFrameRate = 1.0f;
@@ -75,11 +68,30 @@ namespace GameAware {
         [Tooltip("The number of tween frames per second")]
         public float tweenFrameRate = 24f;
 
+        public bool Recording { get; private set; }
+
+        public bool Connected { get { return redDb != null; } }
+
+        public string LastKeyFrameSent { get; private set; }
+
+        public IReadOnlyList<IMetaDataTrackable> CurrentTrackables { 
+            get {
+                return keyItems.AsReadOnly();
+            }
+        } 
+
+        private float CurrentTime {
+            get {
+                return updateMode == RecordingUpdate.FixedUpdate ? Time.fixedTime : Time.time;
+            }
+        }
+
         public enum DebugSetting {
             None,
             All,
             ConstantsOnly
         }
+        
         [Tooltip("Controls debug printing. All=prints everything, None=turns off everything, ConstantsOnly=only prints start_frame, latest, and end_frame")]
         public DebugSetting debugSetting = DebugSetting.All;
 
@@ -97,16 +109,10 @@ namespace GameAware {
         }
 
         IEnumerator Start() {
-            yield return new WaitForEndOfFrame();
-            if (connectOnStart) {
-                InitConnection();
-                yield return StartMetaData();
+            if (BeginMetaDataOnStart) { 
+                yield return new WaitForEndOfFrame();
+                yield return StartMetaDataCoroutine();
             }
-            SnapKeyFrame();
-        }
-
-        private void WriteMetaData(string key, string message) {
-            WriteMetaData(key, message, true);
         }
 
         void InitConnection() {
@@ -122,11 +128,66 @@ namespace GameAware {
             redDb = redisConn.GetDatabase();
         }
 
-        private void WriteMetaData(string key, string message, bool asynchronous) {
 
-            /*if (redDb == null) { InitConnection(); }*/
+        public void StartMetaData() {
+            if (!Connected && !Recording) {
+                StartCoroutine(StartMetaDataCoroutine());
+            }
+        }
 
-            if (redDb != null && recording) {
+        IEnumerator StartMetaDataCoroutine() {
+            if (!Connected) {
+                InitConnection();
+            }
+            while (!Connected) {
+                yield return new WaitForEndOfFrame();
+            }
+            var startMessage = new JObject {
+                {"game_name", gameName },
+                {"streamer_name", streamerName },
+                {"key_frame_rate", keyFrameRate },
+                {"tween_frame_rate", tweenFrameRate },
+                {"game_time", (int)CurrentTime/1000 },
+                {"clock_mills",DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() },
+                {"screen_width", Screen.width },
+                {"screen_height", Screen.height },
+            };
+            string mess = JsonConvert.SerializeObject(startMessage);
+            PublishMessageToMiddleware(PUB_SUB_CHANNEL, "start");
+            WriteMetaDataToMiddleware(START_FRAME, mess, false);
+
+            Recording = true;
+            SnapKeyFrame();
+        }
+
+        public void StopMetaData() {
+            Recording = false;
+            var endMessage = new JObject {
+                {"final_frame_num", keyFrameNum },
+                {"game_time", (int)CurrentTime/1000 },
+                {"clock_mills",DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() },
+            };
+            string mess = JsonConvert.SerializeObject(endMessage);
+            WriteMetaDataToMiddleware(END_FRAME, mess, false);
+            PublishMessageToMiddleware(PUB_SUB_CHANNEL, "end");
+            redDb = null;
+            if (redisConn != null) {
+                redisConn.Close();
+            }
+        }
+
+        /// <summary>
+        /// The principle here is to allow for a user to pause the metaData system from sending data for some time.
+        /// The immediate flag is to allow for letting the current keyframe playout (immediate=false) or if it should stop everything now (immediate=true) this would require changes throughout the system.
+        /// </summary>
+        /// <param name="immediate"></param>
+        public void PauseMetaData(bool immediate=false) {
+            throw new NotImplementedException("Haven't implemented metadata pausing yet.");
+        }
+
+
+        private void WriteMetaDataToMiddleware(string key, string message, bool asynchronous) {
+            if (Connected) {
                 if (asynchronous) {
                     switch (debugSetting) {
                         case DebugSetting.All:
@@ -139,6 +200,9 @@ namespace GameAware {
                             break;
                     }
                     redDb.StringSetAsync(key, message, flags: CommandFlags.FireAndForget);
+                    if(key == LATEST_FRAME) {
+                        LastKeyFrameSent = message;
+                    }
                 }
                 else {
                     switch (debugSetting) {
@@ -152,79 +216,23 @@ namespace GameAware {
                             break;
                     }
                     redDb.StringSet(key, message);
+                    if (key == LATEST_FRAME) {
+                        LastKeyFrameSent = message;
+                    }
                 }
             }
         }
 
-        private void PublishMetaData(string channel, string message) {
-            /*if (redDb == null) {
-                InitConnection();
-            }*/
-            /*ConfigurationOptions config = new ConfigurationOptions {
-                EndPoints = {
-                    { metaDataURL, metaDataPort },
-                },
-                KeepAlive = metaDataKeepAlive,
-                Password = metaDataPassword
-            };
-
-            redisConn = ConnectionMultiplexer.Connect(config);
-            redDb = redisConn.GetDatabase();*/
-            if (redDb != null && recording) {
+        private void PublishMessageToMiddleware(string channel, string message) {
+            if (Connected) {
                 redDb.Publish(channel, message);
             }
         }
 
-        public IEnumerator StartMetaData() {
-            while(redDb == null) {
-                yield return new WaitForEndOfFrame();
-            }
-            var startMessage = new JObject {
-                {"game_name", gameName },
-                {"streamer_name", streamerName },
-                {"key_frame_rate", keyFrameRate },
-                {"tween_frame_rate", tweenFrameRate },
-                {"game_secs", CurrentTime },
-                {"clock_mills",DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() },
-                {"screen_width", Screen.width },
-                {"screen_height", Screen.height },
-            };
-            string mess = JsonConvert.SerializeObject(startMessage);
-            recording = true;
-            PublishMetaData(PUB_SUB_CHANNEL, "start");
-            WriteMetaData(START_FRAME, mess);
-        }
 
-        public void EndMetaDataConnection() {
-            var endMessage = new JObject {
-                {"final_frame_num", keyFrameNum },
-                {"game_secs", CurrentTime },
-                {"clock_mills",DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() },
-            };
-            string mess = JsonConvert.SerializeObject(endMessage);
-            WriteMetaData(END_FRAME, mess);
-            PublishMetaData(PUB_SUB_CHANNEL, "end");
-        }
 
-        /** General Data Schema
-         * keyFrameNum: {"game_time":float,
-         *               "frame_num":int,
-         *               "key":{"obj_id1":{...}, "obj_id2":{...}, ...}, 
-         *               "tweens": [{"dt":float, "game_time":float, "obj_id1":{...}, "obj_id2":{...}, ...},
-         *                          {"dt":float, "game_time":float, "obj_id1":{...}, "obj_id2":{...}, ...}]}
-         *                    
-         * If there are no Tween objects being recorded or if they report nothing then the tween list might be missing
-         * keyFrameNum: {"key":{"obj_id1":{}, "obj_id2":{}, ...}}
-         * 
-         * Possible Future Schema
-         * keyFrameNum: {"key":{"obj_id1":{...}, "obj_id2":{...}, ...}, 
-         *               "tweens": [{"obj_id1":{...}, "obj_id2":{...}, ...},
-         *                          {"obj_id1":{...}, "obj_id2":{...}, ...}],
-         *               "events": [{"event1"}, {"event2"}]}
-         * 
-         */
         void FixedUpdate() {
-            if (updateMode != RecordingUpdate.FixedUpdate || !recording) return;
+            if (updateMode != RecordingUpdate.FixedUpdate || !Recording) return;
 
             if (Time.fixedTime - lastKeyTime > 1 / keyFrameRate) {
                 SendKeyFrame();
@@ -235,7 +243,7 @@ namespace GameAware {
         }
 
         void Update() {
-            if (updateMode != RecordingUpdate.Update || !recording) return;
+            if (updateMode != RecordingUpdate.Update || !Recording) return;
 
             if (Time.time - lastKeyTime > 1 / keyFrameRate) {
                 SendKeyFrame();
@@ -246,7 +254,7 @@ namespace GameAware {
         }
 
         void LateUpdate() {
-            if (updateMode != RecordingUpdate.LateUpdate || !recording) return;
+            if (updateMode != RecordingUpdate.LateUpdate || !Recording) return;
 
             if (Time.time - lastKeyTime > 1 / keyFrameRate) {
                 SendKeyFrame();
@@ -262,22 +270,26 @@ namespace GameAware {
                 currentFrameData["tweens"] = tweens;
                 tweens = new JArray();
             }
-            string frameString = JsonConvert.SerializeObject(currentFrameData, serializerSettings);
+            string frameString = JsonConvert.SerializeObject(currentFrameData);
 
-            WriteMetaData(keyFrameNum.ToString(), frameString, true);
-            WriteMetaData(LATEST_FRAME, frameString, true);
+            WriteMetaDataToMiddleware(keyFrameNum.ToString(), frameString, true);
+            WriteMetaDataToMiddleware(LATEST_FRAME, frameString, true);
 
             keyFrameNum += 1;
             SnapKeyFrame();
         }
 
-
+        /** General Data Schema
+         * keyFrameNum: {"game_time":int, // in milliseconds as browsers encode time
+         *               "frame":int,
+         *               "key":{"obj_id1":{...}, "obj_id2":{...}, ...}, 
+         *               "tweens": [{"dt":int, "game_time":int, "obj_id1":{...}, "obj_id2":{...}, ...},
+         *                          {"dt":int, "game_time":int, "obj_id1":{...}, "obj_id2":{...}, ...}]}
+         *                    
+         * If there are no Tween objects being recorded or if they report nothing then the tween list might be missing
+         * keyFrameNum: {"key":{"obj_id1":{}, "obj_id2":{}, ...}}
+         */
         private void SnapKeyFrame() {
-            if(!recording) {
-                return;
-            }
-
-
             keyItems.AddRange(newItems);
             foreach(IMetaDataTrackable mdo in newItems) {
                 if (mdo.FrameType == MetaDataFrameType.Inbetween) {
@@ -287,7 +299,7 @@ namespace GameAware {
             newItems.Clear();
 
             currentFrameData = new JObject {
-                {"game_time", CurrentTime },
+                {"game_time", (int)CurrentTime/1000 },
                 {"frame", keyFrameNum }
             };
             JObject key = new JObject();
@@ -302,14 +314,14 @@ namespace GameAware {
         }
 
         private void SnapTweenFrame() {
-            if (tweenItems.Count == 0 || !recording) {
+            if (tweenItems.Count == 0 || !Recording) {
                 return;
             }
 
             //inbetweenNum += 1;
             JObject newInbetween = new JObject {
-                    {"dt", CurrentTime - lastTweenTime },
-                    {"game_time", CurrentTime},
+                    {"dt", (int)CurrentTime/1000 - (int)lastTweenTime/1000 },
+                    {"game_time", (int)CurrentTime/1000},
                     //{"frame_num", inbetweenNum }
                 };
             foreach (IMetaDataTrackable mdo in tweenItems) {
@@ -331,10 +343,6 @@ namespace GameAware {
 
         public void AddTrackableObject(IMetaDataTrackable mdo) {
             newItems.Add(mdo);
-            /*keyItems.Add(mdo);
-            if (mdo.FrameType == MetaDataFrameType.Inbetween) {
-                tweenItems.Add(mdo);
-            }*/
         }
 
         public void RemoveTrackableObject(IMetaDataTrackable mdo) {
@@ -343,7 +351,7 @@ namespace GameAware {
         }
 
         void OnDestroy() {
-            EndMetaDataConnection();
+            StopMetaData();
         }
     }
 }
